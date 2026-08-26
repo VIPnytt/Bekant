c188a89d384009a1b0fd0e037d0036764166cae1
Jan-Petter Gundersen <europe.jpg@gmail.com>
2026-08-26 19:35:59 +0000

    Refined README and config templates (#7)
    * Refined README for readability
    
    * Added platformio.ini comments
    
    * Adjusted README + refined secrets.h template
    
    * Added assembly tip + tuned MQTT wording

--- include/esp/secrets.h
+++ include/esp/secrets.h
@@ -1,26 +1,50 @@
 #pragma once
 
+/**
+ * Definitions for the Bekant desk project.
+ * https://github.com/VIPnytt/Bekant
+ */
+
 #ifdef ARDUINO_ARCH_ESP32
 
-// #define PIN_ADC 1 // Optional
-// #define PIN_LED 2 // Optional, WaveShare ESP32-C6-Zero-B has a built-in LED on pin 8
-#define PIN_MISO 3
-#define PIN_MOSI 4
-// #define PIN_OE 5 // Optional, for logic level shifter
-#define PIN_RST 6
-#define PIN_SCK 7
-// #define PIN_TPDN 8 // Optional, button down
-// #define PIN_TPUP 9 // Optional, button up
+/**
+ * ESP32 pins, please change to match your board and wiring.
+ */
+#define PIN_MISO 1
+#define PIN_MOSI 2
+#define PIN_RST 3
+#define PIN_SCK 4
 
-#define HOSTNAME "bekant"
+/**
+ * Optional ESP32 pins for additional features.
+ */
+// #define PIN_ADC 5
+// #define PIN_LED 6 // WaveShare ESP32-C6-Zero-B has a built-in LED on pin 8
+// #define PIN_OE 7
+// #define PIN_TPDN 8
+// #define PIN_TPUP 9
 
+/**
+ * Wi-Fi configuration.
+ */
 #define WIFI_SSID "name"
 #define WIFI_KEY "password"
 
+/**
+ * MQTT configuration.
+ */
 #define MQTT_HOST "mqtt.local"
 #define MQTT_USER "username"
 #define MQTT_KEY "password"
 
-// #define OTA_KEY "password" // Optional
+/**
+ * Optional OTA authentication password.
+ */
+// #define OTA_KEY "password"
+
+/**
+ * Hostname advertised on the local network.
+ */
+#define HOSTNAME "bekant"
 
 #endif // ARDUINO_ARCH_ESP32
